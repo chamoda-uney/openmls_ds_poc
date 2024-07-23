a16c851635db1f3b7b888b49031bbb89925216ca
chamoda <chamoda@uney.com>
2024-07-23 15:41:49 +0400

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+  app.enableCors();
 
   await app.listen(3000);
 }
